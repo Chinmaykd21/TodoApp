dbf4fcbc355db06716bcb411b9dedafc8d8adb3f
Chinmaykd21 <chinmaykd21@gmail.com>
2022-07-02 16:28:49 -0400

    Edit todo with new data

--- server/crudData/editTodo.go
+++ server/crudData/editTodo.go
@@ -11,46 +11,37 @@ import (
 )
 
 func EditTodo(ctx context.Context, c *fiber.Ctx, todoId int, todo customDataStructs.Todo, todos []customDataStructs.Todo, collectionTodos *mongo.Collection) (*[]customDataStructs.Todo, error) {
-	var todoToEdit customDataStructs.Todo
-	var editedTodoInSlice []customDataStructs.Todo
+
+	todoToEdit := todo
+	todoAfterUpdate := []customDataStructs.Todo{}
 
 	// Filter
 	filter := bson.D{{Key: "todoId", Value: bson.D{{Key: "$eq", Value: todoId}}}}
 
-	// Find and store the id whose status is to be toggled to struct todo
-	err := collectionTodos.FindOne(ctx, filter).Decode(&todoToEdit)
-
-	if err != nil {
-		errResponse := serverErrors.New(serverErrors.RetreivalError, err.Error())
-		return &editedTodoInSlice, errResponse
-	}
-
 	// Update condition
 	update := bson.D{{Key: "$set", Value: bson.D{
-		{Key: "todoId", Value: todoId},
 		{Key: "title", Value: todoToEdit.Title},
-		{Key: "body", Value: todoToEdit.Body},
-		{Key: "isCompleted", Value: todoToEdit.IsCompleted}}}}
+		{Key: "body", Value: todoToEdit.Body}}}}
 
 	// Update query
 	updateResult, err := collectionTodos.UpdateOne(ctx, filter, update)
 
 	if err != nil {
 		errResponse := serverErrors.New(serverErrors.UpdateError, err.Error())
-		return &editedTodoInSlice, errResponse
+		return &todoAfterUpdate, errResponse
 	}
 
 	if updateResult.ModifiedCount == 0 {
 		errResponse := serverErrors.New(serverErrors.UpdateError, "Modified records are 0. No Records have been updated")
-		return &editedTodoInSlice, errResponse
+		return &todoAfterUpdate, errResponse
 	}
 
 	if updateResult.MatchedCount != updateResult.ModifiedCount {
 		errResponse := serverErrors.New(serverErrors.UpdateError, "Matched & modified records have different counts")
-		return &editedTodoInSlice, errResponse
+		return &todoAfterUpdate, errResponse
 	}
 
-	editedTodoInSlice = append(editedTodoInSlice, todoToEdit)
+	todoAfterUpdate = append(todoAfterUpdate, todoToEdit)
 
-	return &editedTodoInSlice, nil
+	return &todoAfterUpdate, nil
 }
